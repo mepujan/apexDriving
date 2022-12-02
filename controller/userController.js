bd4620e816af76105a48531cffe3576622f2b7aa
Pujan Gautam <mepujan10@gmail.com>
2022-12-01 22:42:08 -0500

    [updated] GetBookingById method to populate the instructor field from foreign key

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,6 @@
 import user from '../model/User.js';
-import scheduled from '../model/Schedule.js'
+import Schedule from '../model/Schedule.js'
+import mongoose from 'mongoose';
 
 export const GetAllUsers = async(req,res,next)=>{
     try{
@@ -23,7 +24,32 @@ export const GetUserById = async(req,res,next)=>{
 export const GetBookingByStudentId = async(req,res,next)=>{
     try{
         const id = req.userId;
-        const Booking = await scheduled.find({user:id});
+        const Booking = await Schedule.aggregate([
+            {$match:{user: mongoose.Types.ObjectId(id)}},
+            {
+                $lookup:{
+                    from : 'instructors',
+                    localField : 'instructor',
+                    foreignField: '_id',
+                    as:'instructor',
+                    pipeline :[{
+                        $project:{
+                            "_id":0,
+                            "__v":0
+                        }
+                    }
+                    ]
+                }
+            },
+            {
+                $unwind: "$instructor"
+            },{
+                $project:{
+                "__v":0,
+                "user":0
+                }
+            }
+        ]);
         return res.status(200).json(Booking);
     }catch(error){
             next(error);
