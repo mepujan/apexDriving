669fd960e1dbb254a961f1cd70f565259f67c84f
hyleungtommy <104375225+hyleungtommy@users.noreply.github.com>
2022-12-01 19:49:41 -0500

    included instructor detail in /student/booking

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,6 @@
 import user from '../model/User.js';
 import scheduled from '../model/Schedule.js'
+import Instructor from '../model/Instructor.js';
 
 export const GetAllUsers = async(req,res,next)=>{
     try{
@@ -23,10 +24,17 @@ export const GetUserById = async(req,res,next)=>{
 export const GetBookingByStudentId = async(req,res,next)=>{
     try{
         const id = req.params.id;
-        console.log("id=" + id);
         const Booking = await scheduled.find({user:id});
-        console.log(Booking);
-        return res.status(200).json(Booking);
+        const instructors = await Instructor.find({});
+        const MappedBooking = Booking.map(b=>{
+            return {
+                booked_schedule:b.booked_schedule,
+                _id:b._id,
+                user:b.user,
+                instructor:instructors.find(i => i.id == b.instructor)
+            };
+        })
+        return res.status(200).json(MappedBooking);
     }catch(error){
             next(error);
     }
