71f6caebe6a2d1623ca47e9c365cc5abc4ce8287
Pujan Gautam <mepujan10@gmail.com>
2022-12-08 18:53:00 -0500

    [updated] booking schedule

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -6,20 +6,36 @@ import User from '../model/User.js'
 
 export const BookAppointment = async(req,res,next)=>{
     try{
-       const {instructor, booked_scheduled } = req.body;
-       const start_time = new Date(Date.parse(booked_scheduled.start_time));
-       const end_time = new Date(start_time.getTime() + 60 * 60 * 1000);
+        // time should be in format hr:min:sec
+        const weekDays = {
+            "sunday":0,
+            "monday":1,
+            "tuesday":2,
+            "wednesday":3,
+            "thursday":4,
+            "friday":5,
+            "saturday":6
+        }
+        let {instructor,day, time } = req.body;
+        const [hr,min,sec] = time.split(":");
+        const today = new Date();
+        day = day.toLowerCase();
+        const bookingDate = new Date(today.setDate(today.getDate() - today.getDay() + weekDays[day]));
+        bookingDate.setHours(hr);
+        bookingDate.setMinutes(min);
+        bookingDate.setSeconds(sec);
+       const end_time = new Date(bookingDate.getTime() + 60 * 60 * 1000);
        const new_booking = new Schedule({
         user: req.userId,
         instructor: instructor,
         booked_schedule:{
-            start_time:start_time,
+            start_time:bookingDate,
             end_time: end_time
         }
        });
        const result = await new_booking.save();
-       const user = await User.find({_id : req.userId})
-       sendmail(user[0].email,user[0].user_name,start_time)
+       const user = await User.find({_id : req.userId});
+       sendmail(user[0].email,user[0].user_name,bookingDate);
        return res.status(200).json(result);
     }catch(error){
         console.log(error)
